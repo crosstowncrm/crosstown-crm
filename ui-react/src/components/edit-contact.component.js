9a02fd9f3a495cc0a3da5c525e9b5f47498ce4e9
Konstantin Shevchenko <dev.shevchenko.konstantin@gmail.com>
2020-06-16 22:25:34 +0300

    Viewing single Contact: divided into three columns: About, Activity, Associations. Responsive view shows only single column on small screens.

--- ui-react/src/components/edit-contact.component.js
+++ ui-react/src/components/edit-contact.component.js
@@ -4,16 +4,14 @@ import gql from "graphql-tag";
 import "../UserList.css";
 import { withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
-
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Paper,
-  Typography
-} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   root: {
@@ -81,6 +79,15 @@ const GET_CONTACT = gql`
           id
           first_name
       }
+        viewed {
+            timestamp {
+                formatted
+            }
+            Article {
+                id
+                title
+            }
+        }
     }
   }
 `;
@@ -95,123 +102,160 @@ function ContactEdit(props) {
   });
 
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h2" gutterBottom>
-        Contact Edit
-      </Typography>
+      <>
 
       {loading && !error && <p>Loading...</p>}
       {error && !loading && <p>Error</p>}
 
       {data && !loading && !error && (
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell key="first_name">first name</TableCell>
-              <TableCell key="last_name">last name</TableCell>
-              <TableCell key="suffix">suffix</TableCell>
-              <TableCell key="birthday">birthday</TableCell>
-              <TableCell key="contact_emails">contact emails</TableCell>
-              <TableCell key="email">email</TableCell>
-              <TableCell key="phone">phone</TableCell>
-              <TableCell key="mobile">mobile</TableCell>
-              <TableCell key="phone_numbers">phone numbers</TableCell>
-              <TableCell key="linkedin_url">linkedin</TableCell>
-              <TableCell key="facebook_url">facebook</TableCell>
-              <TableCell key="instagram_url">instagram</TableCell>
-              <TableCell key="twitter_url">twitter</TableCell>
-              <TableCell key="lead_status">lead status</TableCell>
-              <TableCell key="lead_type">lead type</TableCell>
-              <TableCell key="lead_date">lead date</TableCell>
-              <TableCell key="lifecycle_stage">lifecycle stage</TableCell>
-              <TableCell key="created_at">created at</TableCell>
-              <TableCell key="last_modified">last modified</TableCell>
-              <TableCell key="last_activity">last activity</TableCell>
-              <TableCell key="last_seen">last seen</TableCell>
-              <TableCell key="first_seen">first seen</TableCell>
-              <TableCell key="email_domain">email domain</TableCell>
-              <TableCell key="marital_status">marital status</TableCell>
-              <TableCell key="companies">companies</TableCell>
-              <TableCell key="owner.first_name">owner name</TableCell>
-              <TableCell key="address.street_address1">street address</TableCell>
-              <TableCell key="properties">properties</TableCell>
-              <TableCell key="listings">listings</TableCell>
-                {/*teams*/}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.Contact.map(contact => {
-              return (
-                <TableRow key={contact.id}>
-                  <TableCell>{contact.first_name}</TableCell>
-                  <TableCell>{contact.last_name}</TableCell>
-                  <TableCell>{contact.suffix ? contact.suffix: "no data"}</TableCell>
-                  <TableCell>{contact.birthday ? contact.birthday: "no data"}</TableCell>
-                  <TableCell>{contact.contact_emails ? contact.contact_emails: "no data"}</TableCell>
-                  <TableCell>{contact.email ? contact.email: "no data"}</TableCell>
-                  <TableCell>{contact.phone ? contact.phone: "no data"}</TableCell>
-                  <TableCell>{contact.mobile ? contact.mobile: "no data"}</TableCell>
-                  <TableCell>{contact.phone_numbers ? contact.phone_numbers: "no data"}</TableCell>
-                  <TableCell>{contact.linkedin_url ? contact.linkedin_url: "no data"}</TableCell>
-                  <TableCell>{contact.facebook_url ? contact.facebook_url: "no data"}</TableCell>
-                  <TableCell>{contact.instagram_url ? contact.instagram_url: "no data"}</TableCell>
-                  <TableCell>{contact.twitter_url ? contact.twitter_url: "no data"}</TableCell>
-                  <TableCell>{contact.lead_status ? contact.lead_status: "no data"}</TableCell>
-                  <TableCell>{contact.lead_type ? contact.lead_type: "no data"}</TableCell>
-                  <TableCell>{contact.lead_date ? contact.lead_date: "no data"}</TableCell>
-                  <TableCell>{contact.lifecycle_stage ? contact.lifecycle_stage: "no data"}</TableCell>
-                  <TableCell>{contact.created_at ? contact.created_at: "no data"}</TableCell>
-                  <TableCell>{contact.last_modified ? contact.last_modified: "no data"}</TableCell>
-                  <TableCell>{contact.last_activity ? contact.last_activity: "no data"}</TableCell>
-                  <TableCell>{contact.last_seen ? contact.last_seen: "no data"}</TableCell>
-                  <TableCell>{contact.first_seen ? contact.first_seen: "no data"}</TableCell>
-                  <TableCell>{contact.email_domain ? contact.email_domain: "no data"}</TableCell>
-                  <TableCell>{contact.marital_status ? contact.marital_status: "no data"}</TableCell>
-                  <TableCell>
-                      {contact.companies.map(company => (
-                          <p>
-                              <Link className="edit-link" to={"/companies/" + company.id}>
-                                  {company.name}
-                              </Link>
-                          </p>
-                      ))}
-                  </TableCell>
-                  <TableCell>
-                    <Link className="edit-link" to={"/users/" + contact.owner.id}>
-                        {contact.owner.first_name}
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                      {/*<Link className="edit-link" to={"/address/" + contact.address.id}>*/}
-                          {contact.address? contact.address.street_address1: "no data"}
-                      {/*</Link>*/}
-                  </TableCell>
-                  <TableCell>
-                      {contact.properties.map(property => (
-                          <p>
-                              <Link className="edit-link" to={"/properties/" + property.id}>
-                                  {property.name}
-                              </Link>
-                          </p>
-                      ))}
-                  </TableCell>
-                  <TableCell>
-                      {contact.listings.map(listing => (
-                          <p>
-                              <Link className="edit-link" to={"/listings/" + listing.id}>
-                                  {listing.name}
-                              </Link>
-                          </p>
-                      ))}
-                  </TableCell>
-                    {/*teams*/}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+
+
+
+          <Grid
+          container
+          spacing={3}
+          style={{
+          border: "3px solid blue",
+          margin: "12px"
+      }}
+          >
+          <Grid item md={8}>
+          <Grid
+          item
+          md={12}
+          style={{
+          border: "2px solid red",
+          margin: "2px"
+      }}
+          >
+              {data.Contact[0].viewed.map(viewed => (
+                  <Link key={viewed.Article.id} className="edit-link" to={"/articles/" + viewed.Article.id}>
+                      {viewed.Article.title} {viewed.timestamp.formatted}
+                  </Link>
+              ))}
+          </Grid>
+          <Grid
+          item
+          md={12}
+          style={{
+          border: "2px solid red",
+          margin: "2px"
+      }}
+          >
+          Activity
+          </Grid>
+          </Grid>
+          <Grid item md={4}>
+          <Grid
+          item
+          md={12}
+          style={{
+          border: "2px solid red",
+          margin: "2px"
+      }}
+          ><Card className={classes.root}>
+          <CardActionArea>
+          <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+          />
+          <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {data.Contact[0].first_name ? data.Contact[0].first_name: "no data"}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {data.Contact[0].description}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          About this Contact
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              <p>Name:</p>
+              <p>{data.Contact[0].first_name}</p>
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              <p>Last name:</p>
+              <p>{data.Contact[0].last_name}</p>
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              <p>email:</p>
+              <p>{data.Contact[0].email}</p>
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              <p>contacted times:</p>
+              <p>{data.Contact[0].contacted_times}</p>
+          </Typography>
+          </CardContent>
+          </CardActionArea>
+          <CardActions>
+          <Button size="small" color="primary">
+          Note
+          </Button>
+          <Button size="small" color="primary">
+          Email
+          </Button>
+          <Button size="small" color="primary">
+          Call
+          </Button>
+          <Button size="small" color="primary">
+          Log
+          </Button>
+          <Button size="small" color="primary">
+          Task
+          </Button>
+          <Button size="small" color="primary">
+          Meet
+          </Button>
+          </CardActions>
+          </Card>
+
+          </Grid>
+          <Grid
+          item
+          md={12}
+          style={{
+          border: "2px solid red",
+          margin: "2px"
+      }}
+          >
+              About
+          </Grid>
+          </Grid>
+          <Grid item md={4}>
+          <Grid
+          item
+          md={12}
+          style={{
+          border: "2px solid red",
+          margin: "2px"
+      }}
+          ><ul>
+              {data.Contact[0].companies.map(company => {
+                  return(
+                      <li>{company.name}</li>
+                  )
+
+              })}
+          </ul>
+
+          </Grid>
+          <Grid
+          item
+          md={12}
+          style={{
+          border: "2px solid red",
+          margin: "2px"
+      }}
+          >
+              Associations
+          </Grid>
+          </Grid>
+          </Grid>
+
       )}
-    </Paper>
+      </>
+
   );
 }
 
